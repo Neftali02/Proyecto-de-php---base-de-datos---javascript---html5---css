cafc6f52bf3ac858260fd7a111646d57c727f26d
Neftali02 <131211439+Neftali02@users.noreply.github.com>
2023-04-18 16:40:02 -0600

    Add files via upload

---
+++ js/imagen.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    setTimeout(function(){
+        $(".content").fadeOut(1200);
+    },1800);
+
+    setTimeout(function(){
+        $(".content2").fadeIn(1200);
+    },2500);
+});
